50674fd7b225762f960b1d9758db8af2146b9cba
Kirill <kirill2008tuy@gmail.com>
2025-02-08 17:39:55 +0200

    n2

--- structbett/structbett.cpp
+++ structbett/structbett.cpp
@@ -24,8 +24,47 @@ void dvoechniki(Student* students, int size) {
         if (poorCount >= 3) cout << students[i].surname << " (" << students[i].group << ")\n";
     }
 }
+//n2
+struct Man {
+    string surname, name;
+    int age;
+    string birthDate;
+};
+void surnameSort(vector<Man>& people) {
+    sort(people.begin(), people.end(), [](Man a, Man b) { return a.surname < b.surname; });
+}
+void birthday(vector<Man>& people, string month) {
+    cout << "birthdays in " << month << ":\n";
+    for (const Man& p : people) {
+        if (p.birthDate.find(month) != string::npos)
+            cout << p.name << " " << p.surname << " - " << p.birthDate << "\n";
+    }
+}
+void findP(vector<Man>& people, string surname, string name) {
+    for (const Man& p : people) {
+        if (p.surname == surname && p.name == name)
+            cout << p.surname << " " << p.name << " - " << p.age << " years, date: " << p.birthDate << "\n";
+    }
+}
+void addPerson(vector<Man>& people, const Man& person) {
+    people.push_back(person);
+}
+void removePerson(vector<Man>& people, string surname, string name) {
+    people.erase(remove_if(people.begin(), people.end(), [&](Man& p) {
+        return p.surname == surname && p.name == name;
+        }), people.end());
+}
+void editPerson(vector<Man>& people, string surname, string name, const Man& updatedPerson) {
+    for (Man& p : people) {
+        if (p.surname == surname && p.name == name) {
+            p = updatedPerson;
+            return;
+        }
+    }
+}
 int main() {
     system("chcp 1251>nul");
+    //n1
     int studentSize = 2;
     Student* students = new Student[studentSize]{
         {"Tuiakhov", "G2", {5, 5, 4, 5, 5}},
@@ -34,4 +73,19 @@ int main() {
     otlichniki(students, studentSize);
     dvoechniki(students, studentSize);
     delete[] students;
+    //n2
+    string month;
+    vector<Man> people = { {"Tuiakhov", "Kyrylo", 16, "01-10-2008"}, {"Ktoto", "Sasha", 23, "01-01-2002"} };
+    surnameSort(people);
+    birthday(people, "10");
+    findP(people, "Tuiakhov", "Kyrylo");
+
+    addPerson(people, {"Napadailo", "Nazar", 25, "05-06-1999" });
+    removePerson(people, "Ktoto", "Sasha");
+    editPerson(people, "Tuiakhov", "Kyrylo", { "Bogatiy", "Kyrylo", 16, "01-10-2008" });
+
+    birthday(people, "06");
+    findP(people, "Napadailo", "Nazar");
+
+    return 0;
 }
